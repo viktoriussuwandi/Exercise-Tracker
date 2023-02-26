b85447705dfce6e9b7d92256663fec2f093af52d
Viktorius S <viktorius.tori@gmail.com>
2023-02-26 15:43:32 +0000

    complete the challenge

--- index.js
+++ index.js
@@ -95,6 +95,49 @@ function gen_id(username) {
   }
 }
 
+//Additional function : load user log as requirement (from, to, and limit)
+function user_log(found_user, from, to, limit) {
+  let check_from = false; let check_to = false; let check_limit = false;
+  if (from)  { check_from  = !isNaN(Date.parse(from)); }
+  if (to)    { check_to    = !isNaN(Date.parse(to));   }
+  if (limit) { check_limit = /^[0-9]+$/.test(limit);   }
+  
+  // console.log( {from : from, to : to, limit : limit } );
+  // console.log( {from : check_from, to : check_to, limit : check_limit } );
+  
+  let _id         = found_user._id;
+  let username    = found_user.username;
+  let count       = parseInt(found_user.count);
+  let log_Exist   = found_user.log;
+  let log_format  = log_Exist.map( (l) => {
+      return { description : l.description, duration : parseInt(l.duration), date : l.date }
+  });
+  let log_date = []; let log_fin  = [];
+
+  //create log_format as date requirement
+  if (check_from == false && check_to == false) { 
+    log_date = log_format; 
+  }
+  else if (check_from == true && check_to == false) {
+    log_date = log_format.filter((d) => { return Date.parse(d.date) > Date.parse(from); } );
+  }
+  else if (check_from == false && check_to == true) {
+    log_date = log_format.filter((d) => { return Date.parse(d.date) < Date.parse(to); } );
+  }
+  else if (check_from == true && check_to == true) {
+    log_date = log_format.filter((d) => {
+      return Date.parse(d.date) > Date.parse(from) && Date.parse(d.date) < Date.parse(to);
+    });
+  }
+
+  if (check_limit == true) { log_fin = log_date.slice(0,limit) }
+  else if (check_limit == false) { log_fin = log_date; }
+  
+  
+  user_data = { _id : _id, username : username, count : count, log : log_fin };
+  return user_data;
+}
+
 app.post('/api/users',
   [ check('username', 'username: Path `username` is required').isLength({ min: 1 }) ],
   (req,res) => {
@@ -122,6 +165,7 @@ app.get('/api/users', (req,res) => {
     return res.json(data);
   }
 });
+
 
 app.post('/api/users/:_id/exercises',
   [
@@ -176,40 +220,28 @@ app.post('/api/users/:_id/exercises',
 
 app.get('/api/users/:_id/logs', (req,res) => {
   Alldata = dataManagement('load data');
-  let id    = req.params._id;
-
+  let id = req.params._id; let {from, to, limit} = req.query;
+  
   if (Alldata === undefined) { return res.json({data : 'no data'}); }
   else {
-    //check if id exist
+    //check if id exist -> load user log
     let id_Exist     = Alldata.map(d => d._id);
     let found_user   = Alldata[ id_Exist.indexOf( id ) ];
     
     if (found_user == undefined) { return res.json({user_id : 'Invalid user id'}); }
     else {
-      let _id        = found_user._id;
-      let username   = found_user.username;
-      let count      = parseInt(found_user.count);
-
-      let log_Exist  = found_user.log;
-      let log_format = log_Exist.map( 
-        (l) => {
-          return {
-          description : l.description,
-          duration    : parseInt(l.duration),
-          date        : l.date
-        }}
-      );
-
-      user_data = { _id : _id, username : username, count : count, log : log_format };
+      user_data = user_log(found_user, from, to, limit);
       return res.json(user_data);
     }
   }
   
 });
+
 /*=========================================================================================*/
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
 
+// log_fin = log_format.filter((d) => { return Date.parse(d.date) > Date.parse(from); } );
 // https://github.com/freeCodeCamp/freeCodeCamp/issues/44806
